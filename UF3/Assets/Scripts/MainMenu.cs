aa8faec090b9a6ecba5cea6c6fbd3def8c593db6
Ernest Gomis <gxmis791@gmail.com>
2021-05-03 13:52:05 +0200

    Main menu funcional, pero la slidebar del sonido no funciona >:v

---
+++ UF3/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ContinueGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+     public void ReturnToMainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+        Debug.Log("Salir");
+    }
+}
