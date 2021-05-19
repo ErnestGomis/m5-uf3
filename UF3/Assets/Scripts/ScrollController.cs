d70094c01d71a0c3c09430ed83f484b2beccd510
Gerardenti <gerard.benito@enti.cat>
2021-05-19 19:45:16 +0200

    Scroll de Cámara acabado, muerte por lentitud y cambios de físicas

---
+++ UF3/Assets/Scripts/ScrollController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollController : MonoBehaviour
+{
+    public float baseSpeed = 0.3f;
+    private Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        float delta = Time.fixedDeltaTime * 1000;
+        rigidBody.velocity = new Vector2(baseSpeed, 0) * delta;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "EndScroll")
+        {
+            baseSpeed = 0;
+        }
+    }
+}
+
